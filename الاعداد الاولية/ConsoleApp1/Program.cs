79a5c2ce2708245582ebf96d65aa1c29fc50ae84
Fareselawady <121154283+Fareselawady@users.noreply.github.com>
2022-12-21 09:21:12 +0200

    Add files via upload

---
+++ الاعداد الاولية/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int i,v,j;
+            Console.WriteLine("MAX:");
+            v  = int.Parse(Console.ReadLine());
+            Console.WriteLine("min:");
+            for ( i = int.Parse(Console.ReadLine()); i <= v; i++)
+            {
+               for (j =2; j <=i; j++)
+                if(i%j==0)
+                    {
+                        
+                        break;
+                    }
+               if (i == j )
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+        }
+    }
+}
